ca45daeffa2d12b5983044e4bde48b8af0861350
Rahulchunduruu <rahulchunduruu@gmail.com>
2026-04-03 13:43:18 +0530

    small changes in the prompt it fix payment_method type

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def build_prompt(problem: str):
         3. If you find any transaction with missing category, highlight it bold
         4. Show the data in red colour where amount is greater than 10000
         5. Show the data in a basic tabular format.
-        6. If i didn't mentioned mode of payment then assume it is credit card else it debit card or cash.
+        6. If i didn't mentioned mode of payment_method then assume it is credit card else it debit card or cash.
         7.if i didn't mentioned subcategory then assume it is general.
     """
 
